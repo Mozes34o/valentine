2122fa0f4b2c691d8c00a0a08f79ad9b764f6f3a
Mozes34o <zmohtashim1@gmail.com>
2026-01-29 01:24:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const response = document.getElementById("response");
+
+yesBtn.addEventListener("click", () => {
+    response.innerHTML = "Excellent Choice Sadaria, Muah 🥰";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const x = Math.random() * 200 - 100;
+    const y = Math.random() * 200 - 100;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+function playMusic() {
+    document.getElementById("bg-music").play();
+}
